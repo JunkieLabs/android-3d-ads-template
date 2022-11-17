ee4d4b442a0adc0568da3c67d76538a35a3bfa78
Sushil <kmrsushil10@gmail.com>
2022-11-17 19:48:09 +0530

    refactored binding

--- app/src/main/java/in/junkielabs/adsmeta/FirstFragment.kt
+++ app/src/main/java/in/junkielabs/adsmeta/FirstFragment.kt
@@ -1,21 +1,19 @@
 package `in`.junkielabs.adsmeta
 
+import `in`.junkielabs.adsmeta.databinding.FragmentFirstBinding
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.ThreeD_Ads.R
-import com.example.ThreeD_Ads.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
