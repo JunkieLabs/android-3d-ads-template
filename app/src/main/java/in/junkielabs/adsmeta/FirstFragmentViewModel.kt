7be29381b1def11477dfd1af13b9c324e36edf76
Sushil <kmrsushil10@gmail.com>
2022-11-23 17:37:39 +0530

    hilt setuped

--- app/src/main/java/in/junkielabs/adsmeta/FirstFragmentViewModel.kt
+++
@@ -1,9 +0,0 @@
-package `in`.junkielabs.adsmeta
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class FirstFragmentViewModel @Inject constructor() :ViewModel() {
-}
