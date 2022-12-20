1762055025d17c3363d32f602150716f3dee2bb0
Sushil <kmrsushil10@gmail.com>
2022-12-20 19:53:43 +0530

    info done

--- app/src/main/java/in/junkielabs/adsmeta/ui/pages/info/InfoFragment.kt
+++ app/src/main/java/in/junkielabs/adsmeta/ui/pages/info/InfoFragment.kt
@@ -2,6 +2,8 @@ package `in`.junkielabs.adsmeta.ui.pages.info
 
 import `in`.junkielabs.adsmeta.databinding.FragmentInfoBinding
 import `in`.junkielabs.adsmeta.ui.base.FragmentBase
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,5 +18,18 @@ class InfoFragment : FragmentBase(true) {
     ): View? {
         vBinding = FragmentInfoBinding.inflate(layoutInflater, container, false)
         return vBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vBinding.buttonGithub.setOnClickListener() {
+            val intent = Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://github.com/JunkieLabs/android-meta-ads"))
+            startActivity(intent)
+        }
+        vBinding.buttonJunkiesLabs.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://junkielabs.in"))
+            startActivity(intent)
+        }
     }
 }
