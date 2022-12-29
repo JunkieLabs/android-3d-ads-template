b274a59eb6c2298e509cae039cbc1e12808169e1
nirajprakash <nirajprakash13@gmail.com>
2022-12-29 20:07:07 +0530

    fixed model

--- app/src/main/java/in/junkielabs/adsmeta/ui/labs/json/model/Model2DNode.kt
+++ app/src/main/java/in/junkielabs/adsmeta/ui/labs/json/model/Model2DNode.kt
@@ -4,9 +4,7 @@ import `in`.junkielabs.adsmeta.domain.template.enitity.ModelGradient
 
 
 data class Model2DNode(
-    val spanCount: Int,
-    val tags: List<ModelTag>,
-    val title: String,
+
     val color: String?,
     val cns: ModelAdCns?,
     val bound: ModelBound?,
